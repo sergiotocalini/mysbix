3cd0755891204015aec3ee694e6a7345fbd9444b
root <root@zabbix.01.amana.vpn>
2018-08-17 07:59:04 +0000

    Updating main function

--- mysbix/mysbix.sh
+++ mysbix/mysbix.sh
@@ -49,24 +49,9 @@ version() {
     exit 1
 }
 
-vert2csv() {
-    sql_raw="${1}"
-
-    IFS="${IFS_DEFAULT}"
-    json_raw=""
-    idx="${#rows[@]}"
-    while read line; do
-	if [[ "${line}" =~ ^\* ]]; then
-	    pos=${idx}
-	    let "idx=idx+1"
-	else
-	    rows[${pos}]+="${line}|"
-	fi
-    done <<< "${sql_raw}"
-    
-    for idx in ${!rows[@]}; do
-    	echo "${rows[${idx}]%?}"
-    done
+zabbix_not_support() {
+    echo "ZBX_NOTSUPPORTED"
+    exit 1
 }
 
 vert2json() {
@@ -94,6 +79,43 @@ vert2json() {
     echo "${json_raw%?} ]" | jq -r "${attrs}" 2>/dev/null
 }
 
+sql_exec() {
+    sql=${1}
+    sql_args=( ${2} )
+    sql_opts=${3}
+    
+    if [[ -f "${sql%.sql}.sql" ]]; then
+	count=1
+	for arg in ${sql_args[@]}; do
+	    params+="SET @p${count}=\"${arg}\";"
+	    let "count=count+1"
+	done
+
+	rval=`mysql --defaults-file=${APP_DIR}/.my.conf \
+                    -s${sql_opts:-N}e "${params}source ${SQL%.sql}.sql;" 2>/dev/null`
+	echo "${rval}"
+	return 0
+    fi
+
+    return 1
+}
+
+join() {
+    delimiter=${1}
+    shift
+    array=( ${@} )
+
+    length=$(( ${#array[@]} - 1 ))
+    for idx in ${!array[@]}; do
+	if [[ ${array[${idx}]} != '' && ${array[${idx}]} != ${delimiter} ]]; then
+	    str+="${array[${idx}]}"
+	    if [[ ${idx} < ${length} ]]; then
+		str+="${delimiter}"
+	    fi
+	fi
+    done
+    echo "${str}"
+}
 #
 #################################################################################
 
@@ -103,13 +125,14 @@ while getopts "s::a:q:s:uphvj:" OPTION; do
 	h)
 	    usage
 	    ;;
-	q)
+	s)
 	    SQL="${APP_DIR}/sql/${OPTARG}"
 	    ;;	    
-	s)
-	    SECTION="${OPTARG}"
+	o)
+	    OUTPUT="${OPTARG}"
 	    ;;
         j)
+	    JSON=1
             IFS=":" JSON_ATTR=( ${OPTARG-:0} )
 	    IFS="${IFS_DEFAULT}"
             ;;
@@ -125,43 +148,47 @@ while getopts "s::a:q:s:uphvj:" OPTION; do
     esac
 done
 
-sql_exec() {
-    sql=${1}
-    sql_args=( ${2} )
-    sql_opts=${3}
-    
-    if [[ -f "${sql%.sql}.sql" ]]; then
-	count=1
-	for arg in ${sql_args[@]}; do
-	    params+="SET @p${count}=\"${arg}\"; "
-	    let "count=count+1"
+if [[ `basename ${SQL%.sql}` =~ replication_(masters|slaves) ]]; then
+    rval=$( sql_exec "${SQL}" "" "E" )
+    [ ${?} != 0 ] && zabbix_not_support
+    if [[ ${#ARGS[*]} > 0 ]]; then
+	for arg in ${ARGS[@]}; do
+	    key=`echo ${arg}|awk -F: '{print $1}'|awk '{$1=$1};1'`
+	    val=`echo ${arg}|awk -F: '{print $2}'|awk '{$1=$1};1'`
+	    if [[ -n ${key} && -n ${val} ]]; then
+		selec[${#selec[@]}]=".${key}==\"${val}\""
+	    else
+		attrs[${#attrs[@]}]="\(.${arg})"
+	    fi
 	done
-
-	rval=`mysql --defaults-file=${APP_DIR}/.my.conf \
-                    -${sql_opts:-N} -se "${params} source ${SQL%.sql}.sql; " 2>/dev/null`
-	echo "${rval}"
-	return 0
+	filters[0]=".[]"
+	filters[1]="select($( join " and" ${selec[@]} ))"
+	filters[2]="\"$( join " | " ${attrs[@]:-'\(.)'} )\""
+	filters=$( join "|" ${filters[@]} )
+    elif [[ `basename ${SQL%.sql}` == 'replication_masters' ]]; then
+	if [[ ${JSON} -eq 1 ]]; then
+	    filters=".[] | \"\(.Master_Host)|\(.Master_UUID)|\(.Master_Server_Id)\""
+	else
+	    filters=".[] | {Master_Host, Master_UUID, Master_Server_Id}"
+	fi
+    elif [[ `basename ${SQL%.sql}` == 'replication_slaves' ]]; then
+	if [[ ${JSON} -eq 1 ]]; then
+	    filters=".[] | \"\(.Host)|\(.Server_id)|\(.Slave_UUID)|\(.Master_id)\""
+	else
+	    filters=".[] | {Host, Server_id, Slave_UUID, Master_id}"
+	fi
     fi
-
-    return 1
-}
-
-discovery() {
-    sql=${1}
-    sql_args=( ${2} )
-
-    if [[ `basename ${sql%.sql}` =~ replication ]]; then
-	rval=$( sql_exec "${sql}" "${sql_args[*]}" "E" )
-	rval=$( vert2json "${rval}" ".[] | \"\(.Host)|\(.Server_id)|\(.Slave_UUID)\"" )
-    else
-	rval=$( sql_exec "${sql}" "${sql_args[*]}" )
+    rval=$( vert2json "${rval}" "${filters}" )
+else
+    rval=$( sql_exec "${SQL}" "${ARGS[*]}" )
+    [ ${?} != 0 ] && zabbix_not_support
+    if [[ `basename ${SQL%.sql}.sql` =~ (global_status|global_variables) ]]; then
+	rval=`echo ${rval} | sed -s "s:^${ARGS[0]} ::"`
     fi
+fi
 
-    echo "${rval}"
-}
 
-if [[ ${SECTION} == 'discovery' ]]; then
-    rval=$(discovery "${SQL}" "${ARGS[*]}" )
+if [[ ${JSON} -eq 1 ]]; then
     echo '{'
     echo '   "data":['
     count=1
@@ -185,52 +212,8 @@ if [[ ${SECTION} == 'discovery' ]]; then
     done <<< ${rval}
     echo '   ]'
     echo '}'
+else
+    echo "${rval:-0}"
 fi
-
-# count=1
-# for arg in ${SQL_ARGS[@]}; do
-#     ARGS+="SET @p${count}=\"${arg//p=}\"; "
-#     let "count=count+1"
-# done
-
-# if [[ -f "${SQL%.sql}.sql" ]]; then
-#     rval=`mysql --defaults-file=${APP_DIR}/.my.conf -sNe "${ARGS} source ${SQL%.sql}.sql;" 2>/dev/null`
-#     if [[ `basename ${SQL%.sql}.sql` =~ (global_status|global_variables) ]]; then
-# 	rval=`echo ${rval} | sed -s "s:^${SQL_ARGS[0]//p=} ::"`
-#     elif [[ `basename ${SQL%.sql}.sql` =~ replication_(masters|slaves).* ]]; then
-# 	rval=$( vert2json "${rval}" "${SQL_ARGS[@]}" )
-#     fi
-#     rcode="${?}"
-#     if [[ ${JSON} -eq 1 ]]; then
-#        echo '{'
-#        echo '   "data":['
-#        count=1
-#        while read line; do
-# 	   if [[ ${line} != '' ]]; then
-#                IFS="|" values=(${line})
-#                output='{ '
-#                for val_index in ${!values[*]}; do
-# 		   output+='"'{#${JSON_ATTR[${val_index}]:-${val_index}}}'":"'${values[${val_index}]}'"'
-# 		   if (( ${val_index}+1 < ${#values[*]} )); then
-#                        output="${output}, "
-# 		   fi
-#                done 
-#                output+=' }'
-#                if (( ${count} < `echo ${rval}|wc -l` )); then
-# 		   output="${output},"
-#                fi
-#                echo "      ${output}"
-# 	   fi
-#            let "count=count+1"
-#        done <<< ${rval}
-#        echo '   ]'
-#        echo '}'
-#     else
-#        echo "${rval:-0}"
-#     fi
-# else
-#     echo "ZBX_NOTSUPPORTED"
-#     rcode="1"
-# fi
 
 exit ${rcode}
